fc961b5980a54a7bfb99593800774bd3c80ddf2e
gunzfox <gunzfox@localhost.localdomain>
2023-04-21 16:56:50 -0400

    Bash scripts

---
+++ prepareShell.sh
@@ -0,0 +1,34 @@
+# Go to home directory
+cd
+
+# Install LSD (version 0.23.1)
+wget https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb
+sudo dpkg -i lsd_0.23.1_amd64.deb
+rm lsd_0.23.1_amd64.deb
+
+# Install Nerd-Fonts
+chmod +x ./installFonts.sh
+./installFonts.sh
+
+# Install bat (run it using 'batcat' command)
+sudo apt install bat
+
+# Install Zsh
+sudo apt install zsh
+
+# Install Oh-My-Zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Install Oh-My-Zsh Plugins
+
+# Zsh-Autocompletion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Zsh-Highlightning
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
+
+# Powerlevel 10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+# Run and configure Powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
